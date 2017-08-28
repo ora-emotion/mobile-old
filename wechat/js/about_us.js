f1b5ca22289c9545c80d97fbe14108fcc2c7f602
jzwgit <706861648@qq.com>
2017-08-28 13:55:30 +0800

    细节修改

--- wechat/js/about_us.js
+++ wechat/js/about_us.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(".btn a").click(function(){
-		$(".module-02-bg").css({"height":"11.5rem","overflow":""});
+		$(".module-02-bg").css({"height":"31rem","overflow":""});
 		$(".btn").hide();
 	});
 })
