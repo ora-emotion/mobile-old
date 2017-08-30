aba4c352525fcf2ddc84e8a532e83ebb55a86191
smpower <bzsjxhywrf@outlook.com>
2017-08-30 11:25:45 +0800

    微信端【关于我们】页面模块二修复左侧图标无法对其的问题

--- wechat/js/about_us.js
+++ wechat/js/about_us.js
@@ -1,7 +1,19 @@
-$(function(){
-	$(".btn a").click(function(){
-		$(".module-02-bg").css({"height":"30rem","overflow":""});
-		$(".btn").hide();
-		$(".module3").css({ "marginTop" : 0 });
-	});
-});
+// $(function(){
+// 	$(".btn a").click(function(){
+// 		$(".module-02-bg").css({"height":"30rem","overflow":""});
+// 		$(".btn").hide();
+// 		$(".module3").css({ "marginTop" : 0 });
+// 	});
+// });
+
+var ViewMore = (function () {
+    var aBtn = $(".module-02 .view-more-btn");
+    var main = $(".module-02 .main");
+    aBtn.click(function (event) {
+
+        $(this).css({ "display" : "none" });
+        main.css({ "height" : "100%", "overflow" : "" });
+
+        return false;
+    });
+}());
