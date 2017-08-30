3f87e43842ce7c2ee200df01f42175dd4729329b
smpower <bzsjxhywrf@outlook.com>
2017-08-30 13:17:32 +0800

    微信端【关于我们】页面调整段落内字体大小

--- wechat/js/about_us.js
+++ wechat/js/about_us.js
@@ -1,19 +1,7 @@
-// $(function(){
-// 	$(".btn a").click(function(){
-// 		$(".module-02-bg").css({"height":"30rem","overflow":""});
-// 		$(".btn").hide();
-// 		$(".module3").css({ "marginTop" : 0 });
-// 	});
-// });
-
-var ViewMore = (function () {
-    var aBtn = $(".module-02 .view-more-btn");
-    var main = $(".module-02 .main");
-    aBtn.click(function (event) {
-
-        $(this).css({ "display" : "none" });
-        main.css({ "height" : "100%", "overflow" : "" });
-
-        return false;
-    });
-}());
+$(function(){
+	$(".btn a").click(function(){
+		$(".module-02-bg").css({"height":"31rem","overflow":""});
+		$(".btn").hide();
+		$(".module3").css({ "marginTop" : 0 });
+	});
+});
